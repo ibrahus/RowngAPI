d15e30dd7d2e410ec97825650ab042275812f9f0
Ibrahim <al-hoseen000@hotmail.com>
2021-08-03 14:46:40 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask, request, abort, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from models import setup_db
+
+
+def create_app(test_config=None):
+
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
+
+    @app.route('/')
+    def health():
+        return jsonify("Healthy")
+
+    return app
+
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
---
+++ models.py
@@ -0,0 +1,41 @@
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from flask_migrate import Migrate
+from datetime import datetime
+from sqlalchemy import Column, String, Integer, create_engine
+import json
+
+database_path = os.environ['DATABASE_URL']
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+class Vendor(db.Model):
+    __tablename__ = 'vendor'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(String)
+    city = db.Column(String)
+    address = db.Column(String)
+    # shows = db.relationship('Show', backref='venue', lazy=True)
+
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'city': self.city,
+            'address': self.address
+        }
